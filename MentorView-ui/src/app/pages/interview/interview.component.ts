94ca18593285ec33747a9e0d7f5784f341b878a3
Hasnain N <hasnainn3666@gmail.com>
2025-03-02 23:02:50 +0000

    Generate pages

---
+++ MentorView-ui/src/app/pages/interview/interview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-interview',
+  standalone: true,
+  imports: [],
+  templateUrl: './interview.component.html',
+  styleUrl: './interview.component.css'
+})
+export class InterviewComponent {
+
+}
