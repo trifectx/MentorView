4c0a601cf72611d304cdf4ee98d8732ffdbaffe9
trifectx <totogodswill12@gmail.com>
2025-03-06 16:48:57 +0000

    start login page doing the authservice.ts add login thing

--- MentorView-ui/src/app/services/auth.service.ts
+++ MentorView-ui/src/app/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { Auth, createUserWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from '@angular/fire/auth';
 import { Observable, from } from 'rxjs';
 
 @Injectable({
@@ -19,6 +19,15 @@ export class AuthService {
             password
         ).then(response => updateProfile(response.user, { displayName: username }));
 
+        return from(promise);
+    }
+
+    login(email: string, password: string): Observable<void> {
+        const promise = signInWithEmailAndPassword(
+            this.firebaseAuth, 
+            email, 
+            password
+        ).then(() => {});
         return from(promise);
     }
 }
